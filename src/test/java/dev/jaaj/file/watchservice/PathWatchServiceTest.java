3ae4e109406ab7fa5a34b504c9e9315636643146
pchopinet <pierre.chopinet97@gmail.com>
2020-06-26 23:55:05 +0200

    create test

---
+++ src/test/java/dev/jaaj/file/watchservice/PathWatchServiceTest.java
@@ -0,0 +1,17 @@
+package dev.jaaj.file.watchservice;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class PathWatchServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
